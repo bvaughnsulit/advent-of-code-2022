6f20ee4b54a9ff7257a3a390ea2256a5d20c06b5
b. vaughn sulit <bvaughnsulit@gmail.com>
2022-12-07 15:50:30 -0800

    day 5

---
+++ 05/2.ts
@@ -0,0 +1,44 @@
+const input = Deno.readTextFileSync('./input.txt')
+  .split('\n\n').map(e => e.split('\n'))
+  // .map((e: string) => e ? Number(e) : undefined )
+
+const stacksStr = input[0].reverse()
+
+const stacks = []
+const start = 1
+const interval = 4
+
+for (let i in stacksStr) {
+  const line = stacksStr[i]
+  for (let col = start; col < line.length; col += interval){
+    if (i === '0') { stacks.push([]) }
+    else {
+      const stackIndex = (col - start) / interval
+      if (line[col] !== ' ') stacks[stackIndex].push(line[col])
+    }
+  }
+}
+
+const moves = input[1].map(e => {
+  if (e) {
+    const data = new RegExp(/^move (\d*) from (\d) to (\d)$/)
+    return data.exec(e).slice(1).map(Number)
+  } 
+})
+
+
+for (const move of moves){
+  if (move) {
+    const count = move[0]
+    const fromStack = stacks[move[1] - 1] 
+    const toStack = stacks[move[2] - 1] 
+
+    toStack.push(...fromStack.splice(-count))
+  }
+}
+
+let result = ''
+stacks.forEach(e => result += e.findLast(() => true))
+
+console.log(stacks)
+console.log(result)
